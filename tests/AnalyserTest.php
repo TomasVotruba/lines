82143bac98e6e4511abf324e2d3e9caf90b02e40
Tomas Votruba <tomas.vot@gmail.com>
2023-07-27 13:43:46 +0200

    misc

--- tests/AnalyserTest.php
+++ tests/AnalyserTest.php
@@ -21,15 +21,6 @@ final class AnalyserTest extends TestCase
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/source.php']);
 
-        //    use PHPUnit\Framework\TestCase;
-        //
-        //        class YourMeasurementTest extends TestCase
-        //        {
-        //            public function testMeasurementResults()
-        //            {
-        //                $measurementResult = ...; // Assuming you have the measurement result object
-
-        // Replace array dim fetch with method call for assertions
         $this->assertSame(1, $measurementResult->getFiles());
         $this->assertSame(82, $measurementResult->getLines());
         $this->assertSame(30, $measurementResult->getLogicalLines());
@@ -56,9 +47,6 @@ final class AnalyserTest extends TestCase
         $this->assertSame(0, $measurementResult->getNonPublicClassConstants());
         $this->assertSame(1, $measurementResult->getGlobalConstants());
         $this->assertSame(0.5, $measurementResult->getAverageFunctionLength());
-        //        $this->assertSame(6, $measurementResult->getMethodCalls());
-        //        $this->assertSame(4, $measurementResult->getStaticMethodCalls());
-        //        $this->assertSame(2, $measurementResult->getNonStaticMethodCalls());
         $this->assertSame(0, $measurementResult->getDirectories());
         $this->assertSame(1, $measurementResult->getNamespaces());
         $this->assertSame(75, $measurementResult->getNonCommentLines());
@@ -71,58 +59,6 @@ final class AnalyserTest extends TestCase
         $this->assertSame(1.3, $measurementResult->getAverageMethodsPerClass());
         $this->assertSame(0, $measurementResult->getMinimumMethodsPerClass());
         $this->assertSame(4, $measurementResult->getMaximumMethodsPerClass());
-        //    }
-        //        }
-
-        //
-        //
-        //        $this->assertEqualsWithDelta(
-        //            [
-        //                'files' => 1,
-        //                'loc' => 82,
-        //                'lloc' => 30,
-        //                'llocClasses' => 28,
-        //                'llocFunctions' => 1,
-        //                'llocGlobal' => 1,
-        //                'expectedCommentLines' => 7,
-        //                'interfaces' => 1,
-        //                'traits' => 0,
-        //                'classes' => 2,
-        //                'functions' => 2,
-        //                'namedFunctions' => 1,
-        //                'anonymousFunctions' => 1,
-        //                'methods' => 4,
-        //                'publicMethods' => 2,
-        //                'nonPublicMethods' => 2,
-        //                'protectedMethods' => 1,
-        //                'privateMethods' => 1,
-        //                'nonStaticMethods' => 3,
-        //                'staticMethods' => 1,
-        //                'constants' => 2,
-        //                'classConstants' => 1,
-        //                'publicClassConstants' => 1,
-        //                'nonPublicClassConstants' => 0,
-        //                'globalConstants' => 1,
-        //                'llocByNof' => 0.5,
-        //                'methodCalls' => 6,
-        //                'staticMethodCalls' => 4,
-        //                'instanceMethodCalls' => 2,
-        //                'directories' => 0,
-        //                'namespaces' => 1,
-        //                'ncloc' => 75,
-        //                'classLlocMin' => 0,
-        //                'classLlocAvg' => 4.0,
-        //                'classLlocMax' => 28,
-        //                'methodLlocMin' => 6,
-        //                'methodLlocAvg' => 7.3,
-        //                'methodLlocMax' => 9,
-        //                'averageMethodsPerClass' => 1.3,
-        //                'minimumMethodsPerClass' => 0,
-        //                'maximumMethodsPerClass' => 4,
-        //            ],
-        //            $measurementResult,
-        //            0.1
-        //        );
     }
 
     #[DataProvider('issue126Provider')]
@@ -143,7 +79,7 @@ final class AnalyserTest extends TestCase
         return [[1, 1], [2, 1], [3, 1], [4, 2], [5, 3], [6, 3], [7, 3]];
     }
 
-    public function testIssue138IsFixed(): void
+    public function testSkipAnonymousClass(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/issue_138.php']);
         $this->assertSame(1, $measurementResult->getClasses());
@@ -167,7 +103,7 @@ final class AnalyserTest extends TestCase
         $this->assertSame(0, $measurementResult->getNotInClassesOrFunctions());
     }
 
-    public function test_it_makes_a_distinction_between_public_and_non_public_class_constants(): void
+    public function testConstAndPublicClassConst(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/class_constants.php']);
 
@@ -178,13 +114,13 @@ final class AnalyserTest extends TestCase
         $this->assertSame(5, $measurementResult->getConstants());
     }
 
-    public function test_it_collects_the_number_of_final_non_final_and_abstract_classes(): void
+    public function testClasses(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/classes.php']);
         $this->assertSame(9, $measurementResult->getClasses());
     }
 
-    public function test_it_makes_a_distinction_between_protected_and_private_methods(): void
+    public function testMethodVisibility(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/methods.php']);
 
@@ -194,7 +130,7 @@ final class AnalyserTest extends TestCase
         $this->assertSame(6, $measurementResult->getMethods());
     }
 
-    public function test_it_provides_average_minimum_and_maximum_number_of_methods_per_class(): void
+    public function testAverageMethodsPerClass(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/methods_per_class.php']);
 
@@ -204,7 +140,7 @@ final class AnalyserTest extends TestCase
         $this->assertSame(4, $measurementResult->getMaximumMethodsPerClass());
     }
 
-    public function test_use_trait_is_not_counted_as_logical_line(): void
+    public function testSkipTraitFromLogicalLines(): void
     {
         $measurementResult = $this->analyser->countFiles([__DIR__ . '/Fixture/class_using_trait.php']);
         $this->assertSame(1, $measurementResult->getClassLines());

970e2a2dc2407166c635b954d2ea0e0fcd371b31
Tomas Votruba <tomas.vot@gmail.com>
2023-07-28 11:33:07 +0200

    comments lines (#12)

--- src/Console/OutputFormatter/JsonOutputFormatter.php
+++ src/Console/OutputFormatter/JsonOutputFormatter.php
@@ -14,12 +14,56 @@ final class JsonOutputFormatter implements OutputFormatterInterface
     public function printResult(Measurements $measurements, OutputInterface $output): void
     {
         $arrayData = [
-            'directories' => $measurements->getDirectories(),
-            'files' => $measurements->getFiles(),
-        ];
+            'filesystem' => [
+                'directories' => $measurements->getDirectories(),
+                'files' => $measurements->getFiles(),
+            ],
 
-        // @todo
-        //$completeReport = array_merge($directories, $measurements);
+            'lines_of_code' => [
+                'code' => $measurements->getNonCommentLines(),
+                'code_relative' => $measurements->getNonCommentLinesRelative(),
+                'comments' => $measurements->getCommentLines(),
+                'comments_relative' => $measurements->getCommentLinesRelative(),
+            ],
+            'lengths' => [
+                'class_max' => $measurements->getMaxClassLength(),
+                'class_average' => $measurements->getAverageClassLength(),
+                'method_max' => $measurements->getMaxMethodLength(),
+                'method_average' => $measurements->getAverageMethodLength(),
+            ],
+            'structure' => [
+                'namespaces' => $measurements->getNamespaces(),
+                'classes' => $measurements->getClassCount(),
+                'interfaces' => $measurements->getInterfaceCount(),
+                'traits' => $measurements->getTraitCount(),
+                'constants' => $measurements->getConstantCount(),
+                'methods' => $measurements->getMethodCount(),
+                'functions' => $measurements->getFunctionCount(),
+            ],
+
+            'methods' => [
+                'non_static' => $measurements->getNonStaticMethods(),
+                'non_static_relative' => $measurements->getNonStaticMethodsRelative(),
+                'static' => $measurements->getStaticMethods(),
+                'static_relative' => $measurements->getStaticMethodsRelative(),
+                'public' => $measurements->getPublicMethods(),
+                'public_relative' => $measurements->getPublicMethodsRelative(),
+                'protected' => $measurements->getProtectedMethods(),
+                'protected_relative' => $measurements->getProtectedMethodsRelative(),
+                'private' => $measurements->getPrivateMethods(),
+                'private_relative' => $measurements->getPrivateMethodsRelative(),
+            ],
+
+            'constants' => [
+                'class' => $measurements->getClassConstants(),
+                'class_public' => $measurements->getPublicClassConstants(),
+                'class_public_relative' => $measurements->getPublicClassConstantsRelative(),
+                'class_non_public' => $measurements->getNonPublicClassConstants(),
+                'class_non_public_relative' => $measurements->getNonPublicClassConstantsRelative(),
+                'global' => $measurements->getGlobalConstantCount(),
+                'global_relative' => $measurements->getGlobalConstantCountRelative(),
+            ],
+        ];
 
         $jsonString = json_encode($arrayData, JSON_PRETTY_PRINT);
         Assert::string($jsonString);

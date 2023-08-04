81fb1c2d77dd646e13e2331ff8df42331878c504
Tomas Votruba <tomas.vot@gmail.com>
2023-08-04 11:57:25 +0200

    em

--- src/Analyser.php
+++ src/Analyser.php
@@ -17,18 +17,6 @@ final class Analyser
     public function __construct(
         private readonly Parser $parser,
     ) {
-        // define fallback constants for PHP 8.0 tokens in case of e.g. PHP 7.2 run
-        if (! defined('T_MATCH')) {
-            define('T_MATCH', 5000);
-        }
-
-        if (! defined('T_READONLY')) {
-            define('T_READONLY', 5010);
-        }
-
-        if (! defined('T_ENUM')) {
-            define('T_ENUM', 5015);
-        }
     }
 
     /**

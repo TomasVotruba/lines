96cfa43dc10e2d5a0018623e0a880c4d705510ea
Francisco Madeira <xico2k@gmail.com>
2023-08-09 12:41:00 +0100

    remove: `TablePrinter`.

--- src/Console/OutputFormatter/TextOutputFormatter.php
+++ src/Console/OutputFormatter/TextOutputFormatter.php
@@ -5,19 +5,18 @@ declare(strict_types=1);
 namespace TomasVotruba\Lines\Console\OutputFormatter;
 
 use Symfony\Component\Console\Output\OutputInterface;
-use TomasVotruba\Lines\Console\TablePrinter;
 use TomasVotruba\Lines\Contract\OutputFormatterInterface;
+use TomasVotruba\Lines\Helpers\NumberFormat;
 use TomasVotruba\Lines\Measurements;
+use function Termwind\render;
+use function Termwind\renderUsing;
 
 final class TextOutputFormatter implements OutputFormatterInterface
 {
-    public function __construct(
-        private readonly TablePrinter $tablePrinter,
-    ) {
-    }
-
     public function printMeasurement(Measurements $measurements, OutputInterface $output, bool $isShort): void
     {
+        renderUsing($output);
+
         $this->printFilesAndDirectories($measurements);
         $this->printLinesOfCode($measurements);
 
@@ -35,23 +34,28 @@ final class TextOutputFormatter implements OutputFormatterInterface
 
     private function printFilesAndDirectories(Measurements $measurements): void
     {
-        $tableRows = [
-            ['Directories', $measurements->getDirectoryCount()],
-            ['Files', $measurements->getFileCount()],
-        ];
-
-        $this->tablePrinter->printItemValueTable($tableRows, 'Metric', 'Count');
+        $this->renderView('table', [
+            'title' => 'Metric',
+            'label' => 'Count',
+            'rows' => $this->formatRows([
+                ['Directories', $measurements->getDirectoryCount()],
+                ['Files', $measurements->getFileCount()],
+            ]),
+        ]);
     }
 
     private function printLinesOfCode(Measurements $measurements): void
     {
-        $tableRows = [
-            ['Code', $measurements->getNonCommentLines(), $measurements->getNonCommentLinesRelative()],
-            ['Comments', $measurements->getCommentLines(), $measurements->getCommentLinesRelative()],
-            ['Total', $measurements->getLines(), 100.0],
-        ];
-
-        $this->tablePrinter->printItemValueTable($tableRows, 'Lines of code', 'Count', true);
+        $this->renderView('table', [
+            'title' => 'Lines of code',
+            'label' => 'Count',
+            'includeRelative' => true,
+            'rows' => $this->formatRows([
+                ['Code', $measurements->getNonCommentLines(), $measurements->getNonCommentLinesRelative()],
+                ['Comments', $measurements->getCommentLines(), $measurements->getCommentLinesRelative()],
+                ['Total', $measurements->getLines(), 100.0],
+            ]),
+        ]);
     }
 
     private function printMethods(Measurements $measurements): void
@@ -60,28 +64,75 @@ final class TextOutputFormatter implements OutputFormatterInterface
             return;
         }
 
-        $this->tablePrinter->printItemValueTable([
-            ['Non-static', $measurements->getNonStaticMethods(), $measurements->getNonStaticMethodsRelative()],
-            ['Static', $measurements->getStaticMethods(), $measurements->getStaticMethodsRelative()],
-            [],
-            ['Public', $measurements->getPublicMethods(), $measurements->getPublicMethodsRelative()],
-            ['Protected', $measurements->getProtectedMethods(), $measurements->getProtectedMethodsRelative()],
-            ['Private', $measurements->getPrivateMethods(), $measurements->getPrivateMethodsRelative()],
-        ], 'Methods', 'Count', true);
+        $this->renderView('table', [
+            'title' => 'Methods',
+            'label' => 'Count',
+            'includeRelative' => true,
+            'rows' => $this->formatRows([
+                ['Non-static', $measurements->getNonStaticMethods(), $measurements->getNonStaticMethodsRelative()],
+                ['Static', $measurements->getStaticMethods(), $measurements->getStaticMethodsRelative()],
+                [],
+                ['Public', $measurements->getPublicMethods(), $measurements->getPublicMethodsRelative()],
+                ['Protected', $measurements->getProtectedMethods(), $measurements->getProtectedMethodsRelative()],
+                ['Private', $measurements->getPrivateMethods(), $measurements->getPrivateMethodsRelative()],
+            ]),
+        ]);
     }
 
     private function printStructure(Measurements $measurements): void
     {
-        $this->tablePrinter->printItemValueTable([
-            ['Namespaces', $measurements->getNamespaceCount()],
-            ['Classes', $measurements->getClassCount()],
-            ['* Constants', $measurements->getClassConstantCount(), null, true],
-            ['* Methods', $measurements->getMethodCount(), null, true],
-            ['Interfaces', $measurements->getInterfaceCount()],
-            ['Traits', $measurements->getTraitCount()],
-            ['Enums', $measurements->getEnumCount()],
-            ['Functions', $measurements->getFunctionCount()],
-            ['Global constants', $measurements->getGlobalConstantCount()],
-        ], 'Structure', 'Count');
+        $this->renderView('table', [
+            'title' => 'Structure',
+            'label' => 'Count',
+            'rows' => $this->formatRows([
+                ['Namespaces', $measurements->getNamespaceCount()],
+                ['Classes', $measurements->getClassCount()],
+                ['* Constants', $measurements->getClassConstantCount(), null, true],
+                ['* Methods', $measurements->getMethodCount(), null, true],
+                ['Interfaces', $measurements->getInterfaceCount()],
+                ['Traits', $measurements->getTraitCount()],
+                ['Enums', $measurements->getEnumCount()],
+                ['Functions', $measurements->getFunctionCount()],
+                ['Global constants', $measurements->getGlobalConstantCount()],
+            ]),
+        ]);
+    }
+
+    /**
+     * @param array<array{0?: string, 1?: int|float, 2?: float|null, 3?: bool}> $rows
+     * @return array<array{}|array{name: string, count: int|float|string, percent: float|string|null, isChild: bool}>>
+     */
+    private function formatRows(array $rows): array
+    {
+        return array_map(static function (array $row): array {
+            if ($row === []) {
+                return [];
+            }
+
+            return [
+                'name' => $row[0],
+                // make big numbers separated with space, e.g. "1 234"
+                'count' => NumberFormat::pretty($row[1]),
+                // optional float relatives
+                'percent' => isset($row[2]) ? NumberFormat::percent($row[2]) : null,
+                'isChild' => $row[3] ?? false,
+            ];
+        }, $rows);
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    private function renderView(string $view, array $data): void
+    {
+        extract($data);
+
+        ob_start();
+
+        include __DIR__ . sprintf('/../views/%s.php', $view);
+
+        render((string) ob_get_contents());
+
+        ob_end_clean();
     }
 }
--- src/Console/views/table.php
+++ src/Console/views/table.php
@@ -1,22 +1,23 @@
-<?php declare(strict_types=1);
+<?php
 
-/** @var string $titleHeader */
-/** @var string $countHeader */
-/** @var int $includeRelative */
-/** @var array<array{0?: string, 1?: int|float|string, 2?: float|string|null, 3?: bool}> $rows */
+declare(strict_types=1);
+
+/** @var string $title */
+/** @var string $label */
+/** @var ?bool $includeRelative */
+/** @var array<array{}|array{name: string, count: int|float|string, percent: float|string|null, isChild: bool}> $rows */
 ?>
 <div class="mt-1 mx-2 max-w-70">
     <div class="flex justify-between">
         <span class="text-green font-bold">
-            <?php echo $titleHeader; ?>
+            <?php echo $title; ?>
         </span>
         <span class="lowercase">
-            <?php echo $countHeader; ?>
-            <?php if ($includeRelative !== 0) { ?>
+            <?php echo $label; ?>
+            <?php if ($includeRelative ?? false) { ?>
                 <span class="text-gray mr-1">/</span>
                 <span class="text-gray font-bold">Relative</span>
-            <?php }
-            ?>
+            <?php } ?>
         </span>
     </div>
     <?php foreach ($rows as $row) { ?>
@@ -24,15 +25,17 @@
             <div />
         <?php } else { ?>
             <div class="flex space-x-1">
-                <span class="<?php echo isset($row[3]) ? 'ml-1' : '' ?>">
-                    <?php echo $row[0] ?>
+                <span class="<?php echo $row['isChild'] ? 'ml-1' : '' ?>">
+                    <?php echo $row['name']; ?>
                 </span>
                 <span class="flex-1 content-repeat-[.] text-gray" />
-                <span><?php echo $row[1] ?></span>
-                <?php if (isset($row[2])) { ?>
+                <span><?php echo $row['count']; ?></span>
+                <?php if ($row['percent']) { ?>
                     <span>
                         <span class="text-gray mr-1">/</span>
-                        <span class="text-gray w-6 text-right"><?php echo $row[2] ?></span>
+                        <span class="text-gray w-6 text-right">
+                            <?php echo $row['percent']; ?>
+                        </span>
                     </span>
                 <?php } ?>
             </div>

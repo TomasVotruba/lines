f0eac54d8bf6922f6b5abf0b8054af86402fb36b
Tomas Votruba <tomas.vot@gmail.com>
2025-12-04 14:16:49 +0100

    misc

--- src/FeatureCounter/ResultPrinter.php
+++ src/FeatureCounter/ResultPrinter.php
@@ -23,39 +23,21 @@ final readonly class ResultPrinter
         $rows = [];
 
         $previousPhpVersion = null;
-        $lastItemKey = array_key_last($featureCollector->getPhpFeatures());
-        foreach ($featureCollector->getPhpFeatures() as $key => $phpFeature) {
 
-            //            foreach ($phpFeatures as $phpFeature) {
+        foreach ($featureCollector->getPhpFeatures() as $key => $phpFeature) {
+            $changedPhpVersion = $previousPhpVersion !== null && $previousPhpVersion !== $phpFeature->getPhpVersion();
+            if ($changedPhpVersion) {
+                // add empty breakline
+                $rows[] = new TableSeparator();
+            }
+
             $rows[] = [
                 '<fg=yellow>' . $phpFeature->getPhpVersion() . '</>',
                 str_pad($phpFeature->getName(), 45, ' ', STR_PAD_RIGHT),
                 str_pad(number_format($phpFeature->getCount(), 0, ',', ' '), 10, ' ', STR_PAD_LEFT)];
-            //            }
 
-            $changedPhpVersion = $previousPhpVersion !== null && $previousPhpVersion !== $phpFeature->getPhpVersion();
             $previousPhpVersion = $phpFeature->getPhpVersion();
 
-            if ($changedPhpVersion && $lastItemKey !== $key) {
-                // add empty breakline
-                $rows[] = new TableSeparator();
-            }
-        }
-
-        $this->symfonyStyle->table(['PHP', 'Feature', 'Count'], $rows);
-
-        $this->symfonyStyle->newLine();
-
-        $this->symfonyStyle->writeln(
-            sprintf('<fg=yellow>%s=== Summary by PHP version ===</>', str_repeat(' ', 16))
-        );
-        $this->symfonyStyle->newLine();
-
-        $rows = [];
-        foreach ($featureCollector->getFeatureCountByPhpVersion() as $phpVersion => $featureCount) {
-            $rows[] = [
-                str_pad($phpVersion, 40, ' ', STR_PAD_RIGHT),
-                str_pad(number_format($featureCount, 0, ',', ' '), 15, ' ', STR_PAD_LEFT)];
         }
 
         $this->symfonyStyle->table(['PHP version', 'Feature count'], $rows);
